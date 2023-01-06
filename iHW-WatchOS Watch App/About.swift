16c116bab6f11c9b18953276a7e4fc37aed44672
snagglegrolop <snagglegrolop@gmail.com>
2023-01-05 21:39:29 -0800

    Made about section look cleaner and more centered

--- iHW-WatchOS Watch App/About.swift
+++ iHW-WatchOS Watch App/About.swift
@@ -10,27 +10,24 @@ import SwiftUI
 struct About: View {
     var body: some View {
         VStack{
-            Text("Proof of concept made")
-                .multilineTextAlignment(.center)
-                .padding(.top, 0.5)
-                .fontWeight(.light)
-            Text("by Zachary Abrahamson")
-                .multilineTextAlignment(.center)
-                .fontWeight(.light)
-                .padding(.bottom, 0.5)
-            Text("iHW built from the ground up for WatchOS")
-                .multilineTextAlignment(.center)
-                .padding(.bottom, 0)
-                .fontWeight(.light)
-            Text("With the help of Jonathan Damico")
-                .multilineTextAlignment(.center)
-                .padding(.top, 0)
-                .fontWeight(.light)
-            Text("Project started in January of 2022")
-                .multilineTextAlignment(.center)
-                .padding(.top, 0.5)
-                .fontWeight(.light)
-
+            Form {
+                Text("App made by Zachary Abrahamson.")
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 0.5)
+                    .fontWeight(.light)
+                Text("iHW built from the ground up for Apple WatchOS")
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 0)
+                    .fontWeight(.light)
+                Text("With the help of Jonathan Damico")
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(.top, 0)
+                    .fontWeight(.light)
+                Text("Project started in January of 2022")
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(.top, 0.5)
+                    .fontWeight(.light)
+            }
         }
     }
 }
