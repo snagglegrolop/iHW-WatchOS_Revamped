5f32b12fc3a1add844e52782a51daf5b0abc8554
snagglegrolop <snagglegrolop@gmail.com>
2023-01-06 21:14:22 -0800

    I forgot what edits i made

---
+++ iHW-WatchOS Watch App/Schedule.swift
@@ -0,0 +1,21 @@
+//
+//  Schedule.swift
+//  iHW-WatchOS Watch App
+//
+//  Created by Zachary Abrahamson  on 1/6/23.
+//
+
+import SwiftUI
+
+struct Schedule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Schedule_Previews: PreviewProvider {
+    static var previews: some View {
+        Schedule()
+    }
+}
+
--- iHW-WatchOS Watch App/Sign_In.swift
+++ iHW-WatchOS Watch App/Sign_In.swift
@@ -7,27 +7,48 @@
 
 import SwiftUI
 
-
+var logged = false
 
 struct Sign_In: View {
     @State private var username = ""
     @State private var password = ""
-
+    @State var tag : Int? = nil
+    
     var body: some View {
         Form {
             TextField("Username", text: $username)
             SecureField("Password", text: $password)
-            
         }
+        
+        
         .onSubmit {
             guard username.isEmpty == false && password.isEmpty == false else { return }
-            print("Authenticating…")
+            let logged = true
+            print(logged)
+            if logged == true {
+                self.tag = 1
+                print("im a reall boy")
+                NavigationStack {
+                    NavigationLink(destination: Schedule(), tag: 1, selection: $tag) {
+                        EmptyView()
+                    }.disabled(true)
+                }
+            }
+                                       
+            
         }
         .submitLabel(.next)
+        
     }
 }
+
+
+
+
+
 struct UsernamePasswordEnter_Preview: PreviewProvider {
     static var previews: some View {
         Sign_In()
     }
 }
+
